ed04b461fd5207437d268c1bcda9530333875604
NAVNAV123 <52596137+NAVNAV123@users.noreply.github.com>
2019-11-23 11:25:54 +0200

    work, but localy
    I need help at the 'send' function...
    And this is better from the last one, this is work but in 'localhost'...

--- curl_process.py
+++ curl_process.py
@@ -6,14 +6,37 @@ class HTTPRequest(object):
         self.headers = {}
         self.method = {}
         self.url = url
-
+    # def send(self):                  #TODO: check how i can put in the headers also
+    #     self.socket = socket.socket()
+    #     self.connection, _ = self.socket.create_connection((self.url, 443))
+    #     self.streaming()
+    
 x = HTTPRequest("https://google.com/") #TODO: I need to receive client input include the headers
 x.headers["Content-type"] = "application/json"
 x.method["message"] = "GET/auth HTTP/1.1\r\n"
-host = socket.gethostbyname(x.url)
+# x.send() #TODO: refer this to send function
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(host, 443)
-s.sendall(x.method + x.headers + "\r\n")
+s = socket.socket()
+s.bind(('', 8080))
+s.listen(5)
+client, adress = s.accept()
+
+print("Incoming:", adress)
+print(client.recv(1024))
+print
+
+client.send(x.method + '\r\n')
+client.send(x.headers + "\r\n\r\n")
+client.send('<html><body><h1>Hello World</body></html>')
+client.close()
+
+print("Answering ...")
+print("Finished.")
 
 print(s.recv(1024))
+s.close()
+
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(host, 443)
+# s.sendall(x.method + x.headers + "\r\n")
